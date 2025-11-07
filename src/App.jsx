f316ebab21aa4a21255484b7879cedfebb2eed80
Kartyos-India <128411905+Kartyos-India@users.noreply.github.com>
2025-11-07 19:02:51 +0530

    Set up App component with Navbar and Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,14 @@
-sa
+import React from "react";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Home />
+      </main>
+    </>
+  );
+}
